6e1988a4a9867e0287c6be7f5313532225543ae9
Lizzie <lizzie.sorin@hotmail.fr>
2018-12-03 18:27:31 -0800

    startingState public method returning the text

--- Assets/State.cs
+++ Assets/State.cs
@@ -5,5 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "State")]
 public class State : ScriptableObject {
 
-    [SerializeField] string storyText;
+    [TextArea(10, 14)][SerializeField] string storyText;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
 }
