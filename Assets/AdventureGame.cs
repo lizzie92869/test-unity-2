fc9e76dbc06758662ad3e97bc18cd051f4d3af79
Lizzie <lizzie.sorin@hotmail.fr>
2018-12-06 12:08:51 -0800

    adding array

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class AdventureGame : MonoBehaviour {
-
     [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    string[] daysOfTheWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+    State state;
+
 	// Use this for initialization
 	void Start () {
-		textComponent.text = ("The story goes like this...");
+        state = startingState;
+		textComponent.text = state.GetStateStory();
+        Debug.Log(daysOfTheWeek[1]);
 	}
 	
 	// Update is called once per frame
